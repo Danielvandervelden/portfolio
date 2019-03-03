e50cee44bb253f13336bc1ccdc773332e4378b2b
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro-2.local>
2019-03-03 09:21:53 +0000

    Made a store and now doing everything from there.

--- components/admin/AdminNav.vue
+++ components/admin/AdminNav.vue
@@ -1,30 +1,55 @@
 <template>
-	<div class="dv-admin__nav">
-		<ul>
-			<nuxt-link :to="route === 'admin' ? '/admin' : '/admin/' + route" tag="li" v-for="(route, index) in routes" :key="route + index">{{route}}</nuxt-link>
+	<nav class="dv-admin__nav">
+		<ul class="dv-admin__nav-list">
+			<nuxt-link class="dv-admin__nav-list--item" tag="li" to="/">Frontpage</nuxt-link>
+			<nuxt-link class="dv-admin__nav-list--item" :to="route === 'admin' ? '/admin' : '/admin/' + route" tag="li" v-for="(route, index) in getAdminPages" :key="route + index">{{route}}</nuxt-link>
 		</ul>
-	</div>
+	</nav>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				routes: []
+				
+			}
+		},
+		computed: {
+			getAdminPages() {
+				return this.$store.getters['admin/admin/getAdminPages'];
 			}
 		},
 		mounted() {
-			let routes = [...this.$router.options.routes];
-
-			routes.forEach(route => {
-				let actualRoute = route.path.split('/')[route.path.split('/').length -1];
-				if(actualRoute.length > 1) {
-					this.routes.push(actualRoute);
-				}
-			})
+			this.$store.dispatch('admin/admin/getAdminPages');
 		}
 	}
 </script>
 <style scoped lang='scss'>
-	
+	@import '~assets/styles/_base.scss';
+
+	.dv-admin__nav {
+		background-color: $light_grey;
+		@include container();
+		padding: 10px 0;
+
+		&-list {
+			display: flex;
+			justify-content: space-around;
+			list-style: none;
+			padding: 0;
+			margin: 0;
+
+			&--item {
+				cursor: pointer;
+				font-weight: lighter;
+				color: $red;
+				text-transform: uppercase;
+				transition: all .3s ease-in-out;
+
+				&:hover {
+					color: $pink;
+				}
+			}
+		}
+	}
 </style>
--- store/admin/admin.js
+++ store/admin/admin.js
@@ -4,20 +4,38 @@
 
 
 export const state = () => ({
-	loggedIn: false,
+	loggedIn: true,
 	token: null,
+	adminPages: []
 })
 
 export const getters = {
 	loggedIn: state => {
 		return state.loggedIn;
+	},
+	getAdminPages: state => {
+		return state.adminPages;
 	}
 }
 
 export const mutations = {
-
+	getAdminPages(state, pages) {
+		state.adminPages = pages;
+	}
 }
 
 export const actions = {
+	getAdminPages(context) {
+		let routes = [...this.$router.options.routes];
+		let routesArray = [];
 
+		routes.forEach(route => {
+			let actualRoute = route.path.split('/')[route.path.split('/').length -1];
+			if(actualRoute.length > 1) {
+				routesArray.push(actualRoute);
+			}
+		})
+
+		context.commit('getAdminPages', routesArray);
+	}
 }
